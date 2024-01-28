de30ceea48e5a30f09d4a3a2b94eec1eaba74f79
AbidNawaz <abidpart2ics@gmail.com>
2024-01-28 11:13:21 +0500

    removed backend integration

--- src/api/api.js
+++
@@ -1,4 +0,0 @@
-const base_url = `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_API_ENDPOINT}`
-
-export const user_list = `${base_url}/data`;
-
--- src/layout/pages/settings-page/Settings.jsx
+++ src/layout/pages/settings-page/Settings.jsx
@@ -1,20 +1,10 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { user_list } from '../../../api/api'
+import React from 'react'
 
 const Settings = () => {
-  const [data,setData] = useState([])
-  useEffect(() => {
-    fetchUserList()
-  }, [])
-  const fetchUserList = async () => {
-    const response = await axios.get(user_list);
-    response && setData(response.data)
-  }
 
   return (
-    <div style={{padding:20}}>
-      currently logged in as : {data && data.map(item => item.name)}
+    <div style={{ padding: 20 }}>
+      currently logged in as : user
     </div>
   )
 }
