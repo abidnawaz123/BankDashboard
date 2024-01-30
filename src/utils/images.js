92a6a78e6feed12d0150a797bdbf97aa12df74a8
AbidNawaz <abidpart2ics@gmail.com>
2024-01-31 01:14:35 +0500

    created transaction pannel for dashboard

---
+++ src/utils/images.js
@@ -0,0 +1,9 @@
+import moneychain from "../assets/icons/moneychain.svg"
+import paypal from "../assets/icons/paypal.svg"
+import bussiness from "../assets/icons/bussiness.svg"
+
+export const images = {
+    moneychain,
+    paypal,
+    bussiness
+}
