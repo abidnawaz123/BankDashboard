77a7b9efdd8e9c07f4a99516b642963a8afdd1ac
AbidNawaz <abidpart2ics@gmail.com>
2024-02-09 12:34:21 +0500

    integrated api for adding new card

--- src/layout/pages/credit-cards/Add-Card/Formik/form-base.jsx
+++ src/layout/pages/credit-cards/Add-Card/Formik/form-base.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import { validationSchema } from './form-defination/add-card-defination';
-import { Box, Button, Input, TextField } from '@mui/material';
+import { Alert, Box, Button, CircularProgress, TextField, Grid } from '@mui/material';
 import axios from 'axios';
 import styles from "./style.module.scss";
 
 const FormBase = () => {
+    const [response, setResponse] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [state, setState] = React.useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+    });
+    const { vertical, horizontal, open } = state;
+
+
+    const handleClick = (newState) => () => {
+        setState({ ...newState, open: true });
+    };
+
+    const handleClose = () => {
+        setState({ ...state, open: false });
+    };
 
     return (
         <div className={styles.formBaseWrapper}>
@@ -18,15 +35,19 @@ const FormBase = () => {
                 }}
                 validationSchema={validationSchema}
                 onSubmit={(values, { setSubmitting }) => {
-                    console.log('values are =+++>', values)
+                    setIsLoading(true)
                     const sendData = {
                         balance: values.balance,
                         name: values.cardholdername,
                         cardNumber: values.number,
                         valid: values.validThru
                     }
-                    const res = axios.post("http://localhost:4000/api/card", sendData)
-                    res.then(response => console.log('response is =>', response))
+                    const res = axios.post("/api/card", sendData)
+                    res.then(resp => {
+                        setResponse(resp)
+                        setIsLoading(false)
+                        setSubmitting(false)
+                    })
                 }}
             >
                 {({
@@ -53,77 +74,92 @@ const FormBase = () => {
                         setFieldValue("number", e.target.value)
                     }
                     return (
-                        <form onSubmit={handleSubmit}>
-                            <Box className={styles.formBox}>
-                                <TextField
-                                    sx={{
-                                        "& fieldset": {
-                                            border: "1px solid #DFEAF2",
-                                            borderRadius: 2
-                                        },
-                                        minWidth: 250
-                                    }}
-                                    name="balance"
-                                    placeholder='Balance'
-                                    type="text"
-                                    onChange={changeBalanceHandler}
-                                    onBlur={handleBlur}
-                                />
-                                {errors.balance && touched.balance && errors.balance}
-                                <TextField
-                                    sx={{
-                                        "& fieldset": {
-                                            border: "1px solid #DFEAF2",
-                                            borderRadius: 2
-                                        },
-                                        minWidth: 250
-                                    }}
-                                    name="cardholdername"
-                                    placeholder='Cardholdername'
-                                    onChange={changeCardHolderName}
-                                    onBlur={handleBlur}
-                                />
-                                {errors.cardholdername && touched.cardholdername && errors.cardholdername}
+                        <form className={styles.form} onSubmit={handleSubmit}>
+                            <Box className={styles.formBox1}>
+                                <Box className={styles.BalancefieldWrapper}>
+                                    <TextField
+                                        sx={{
+                                            "& fieldset": {
+                                                border: "1px solid #DFEAF2",
+                                                borderRadius: 2
+                                            },
+                                            minWidth: 250
+                                        }}
+                                        name="balance"
+                                        placeholder='Balance'
+                                        type="number"
+                                        onChange={changeBalanceHandler}
+                                        onBlur={handleBlur}
+                                    />
+                                    <Box component="span" color="red">
+                                        {errors.balance && touched.balance && errors.balance}
+                                    </Box>
+                                </Box>
+                                <Box className={styles.cardholderfieldWrapper}>
+                                    <TextField
+                                        sx={{
+                                            "& fieldset": {
+                                                border: "1px solid #DFEAF2",
+                                                borderRadius: 2
+                                            },
+                                            minWidth: 250
+                                        }}
+                                        name="cardholdername"
+                                        placeholder='Cardholdername'
+                                        onChange={changeCardHolderName}
+                                        onBlur={handleBlur}
+                                    />
+                                    <Box component="span" color="red">
+                                        {errors.cardholdername && touched.cardholdername && errors.cardholdername}
+                                    </Box>
+                                </Box>
                             </Box>
-                            <Box style={{ display: "flex", justifyContent: "space-around", marginBottom: "2rem" }}>
-                                <TextField
-                                    sx={{
-                                        "& fieldset": {
-                                            border: "1px solid #DFEAF2",
-                                            borderRadius: 2
-                                        },
-                                        minWidth: 250
-                                    }}
-                                    name="validThru"
-                                    placeholder='ValidThru'
-                                    type="date"
-                                    onChange={changeValidThru}
-                                    onBlur={handleBlur}
-                                />
-                                {errors.validThru && touched.validThru && errors.validThru}
-                                <TextField
-                                    sx={{
-                                        "& fieldset": {
-                                            border: "1px solid #DFEAF2",
-                                            borderRadius: 2
-                                        },
-                                        minWidth: 250
-                                    }}
-                                    name="number"
-                                    placeholder='Number'
-                                    onChange={changeNumber}
-                                    type="text"
-                                    onBlur={handleBlur}
-                                />
-                                {errors.number && touched.number && errors.number}
+                            <Box className={styles.formBox2}>
+                                <Box className={styles.validThrufieldWrapper} >
+                                    <TextField
+                                        sx={{
+                                            "& fieldset": {
+                                                border: "1px solid #DFEAF2",
+                                                borderRadius: 2
+                                            },
+                                            minWidth: 250
+                                        }}
+                                        name="validThru"
+                                        placeholder='ValidThru'
+                                        type="date"
+                                        onChange={changeValidThru}
+                                        onBlur={handleBlur}
+                                    />
+                                    <Box component="span" color="red">
+                                        {errors.validThru && touched.validThru && errors.validThru}
+                                    </Box>
+                                </Box>
+                                <Box className={styles.numberfieldWrapper}>
+                                    <TextField
+                                        sx={{
+                                            "& fieldset": {
+                                                border: "1px solid #DFEAF2",
+                                                borderRadius: 2
+                                            },
+                                            minWidth: 250
+                                        }}
+                                        name="number"
+                                        placeholder='Number'
+                                        onChange={changeNumber}
+                                        type="number"
+                                        onBlur={handleBlur}
+                                    />
+                                    <Box component="span" color="red">
+                                        {errors.number && touched.number && errors.number}
+                                    </Box>
+                                </Box>
                             </Box>
-                            <Box style={{ marginLeft: 75, marginBottom: "2rem" }}>
+                            <Box className={styles.cardAddActionButton}>
                                 <Button type="submit"
-                                    disabled={isSubmitting || !values.number}
-                                    style={{
-                                        backgroundColor: "#1814F3",
-                                        color: "white", minWidth: 150
-                                    }}
+                                    disabled={isSubmitting || !values.number || isLoading}
+                                    className={styles.submitButton}
+                                    endIcon={isLoading &&
+                                        <CircularProgress variant='indeterminate' size={20} />}
                                 >
                                     Add Card
                                 </Button>
--- src/layout/pages/loans/Loans.jsx
+++ src/layout/pages/loans/Loans.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import LoanCards from './Loan-Cards/LoanCards'
+import { Box, Typography } from '@mui/material'
+import { ACTIVE_LOANS } from '../../../utils/constants'
+import { LoanTable } from './Loan-Table/LoanTable'
 
 const Loans = () => {
     return (
-        <div>
-            This is Loans page
-        </div>
+        <React.Fragment>
+            <LoanCards />
+            <Box style={{ padding: 20 }}>
+                <Typography fontWeight="bold" >{ACTIVE_LOANS}</Typography>
+                <LoanTable />
+            </Box>
+        </React.Fragment>
     )
 }
 
